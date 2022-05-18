dd5b1f0fbe329317c2cf545f0f2a5c8fc35eb716
EmNoTrym <63888869+EmNoTrym@users.noreply.github.com>
2022-05-18 16:40:20 +0700

    create_clock and improve_move_feature

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Globalization;
+
+public class Clock : MonoBehaviour
+{
+    private float secDegree;
+    private float minDegree;
+    private float hourDegree;
+
+    public GameObject hour;
+    public GameObject min;
+    public GameObject sec;
+
+
+    public bool isSmooth = false;
+
+    void Update()
+    {
+        if (!isSmooth)
+        {
+            secDegree = -(DateTime.Now.Second / 60f) * 360;
+            minDegree = -(DateTime.Now.Minute / 60f) * 360;
+            hourDegree = -(DateTime.Now.Hour / 12f) * 360;
+
+            sec.transform.rotation = Quaternion.Euler(new Vector3(0, 0, secDegree));
+            min.transform.rotation = Quaternion.Euler(new Vector3(0, 0, minDegree));
+            hour.transform.rotation = Quaternion.Euler(new Vector3(0, 0, hourDegree));
+        }
+        
+        else if (isSmooth)
+        {
+            secDegree = -((DateTime.Now.Second + DateTime.Now.Millisecond / 1000f) / 60f) * 360;       
+            minDegree = -((DateTime.Now.Minute + DateTime.Now.Second / 60f) / 60f) * 360;
+            hourDegree = -((DateTime.Now.Hour + DateTime.Now.Minute / 60f) / 12f) * 360;
+
+            min.transform.rotation = Quaternion.Euler(new Vector3(0, 0, minDegree));
+            sec.transform.rotation = Quaternion.Euler(new Vector3(0, 0, secDegree));
+            hour.transform.rotation = Quaternion.Euler(new Vector3(0, 0, hourDegree));
+
+        }
+    }
+
+    public void ChangeIsSmooth()
+    {
+        isSmooth = !isSmooth;
+    }
+}
